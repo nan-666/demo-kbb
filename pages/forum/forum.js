98c2165f2d3c015298516e33d630c923a3c4c5f8
nan-666 <https://github.com/nan-666>
2020-05-08 16:33:27 +0800

    论坛

--- pages/forum/forum.js
+++ pages/forum/forum.js
@@ -5,7 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    block:[{
+      id: 1,
+      title: "行业干货"
+    },
+    {
+      id: 2,
+      title: "趋势热点"
+    },
+    {
+      id: 3,
+      title: "生活小技巧"
+    },
+    {
+      id: 4,
+      title: "爆款设计"
+    },
+    {
+      id: 5,
+      title:"行业机会"
+    },
+    {
+      id: 6,
+      title:"全部话题"
+    }],
+    backgroundImgUrl:"http://q9xwl365p.bkt.clouddn.com/forum/index/ltbk.png"
   },
 
   /**
