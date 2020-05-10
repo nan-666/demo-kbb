834d24ff7fb2b667e3fa8e7f55cd513776d9b806
nan-666 <https://github.com/nan-666>
2020-05-10 16:56:14 +0800

    3333

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -10,5 +10,18 @@ Page({
   
   btnclick: function(e) {
     wx.navigateTo({ url: '/pages/personal/worker/worker' })
+  },
+  //点击账号设置跳转到设置页
+  toSetting:function(){
+    wx.navigateTo({
+      url: '/pages/Asetting/setting',
+    })
+  },
+  //点击服务号跳转到售后页
+  toAftersale:function(){
+    wx.navigateTo({
+      url: '/pages/personal/property/aftersale/aftersale',
+    })
+
   }
 })
