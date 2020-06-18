77de1e3593ed419413d9927a32b83121618dd603
nan-666 <https://github.com/nan-666>
2020-06-18 15:26:24 +0800

    小改动
    1

--- pages/index/task-detail/detail.js
+++ pages/index/task-detail/detail.js
@@ -32,4 +32,10 @@ Page({
      }
     })
   },
+  toChat:function(e){
+    console.log(e.target.dataset.userid)
+    wx.navigateTo({
+      url: '/pages/chat/chat/chat?toAccount=' + e.target.dataset.userid    // 将聊天对方ID传递到会话页面
+    })
+  }
 })
